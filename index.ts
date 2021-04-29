a4ed2f73dfc4eda4af0953688911ce37718d43de
Petr <solodov.petr@gmail.com>
2021-04-29 23:45:44 +0300

    interface - define object structure example

---
+++ index.ts
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+// define object structure
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+axios.get(url).then((response) => {
+  const todo = response.data as Todo;
+
+  const id = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
+
+  console.log(id, title, completed);
+});
